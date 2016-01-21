ccee57413e312df0bd02ec987eba5aef46983b6a
rmugattarov <rmugattarov@RMugattarov2-PC.datateh.ru>
2016-01-21 19:00:32 +0500

    updates

--- src/main/java/rmugattarov/gwt_test/client/EntryPointClass.java
+++ src/main/java/rmugattarov/gwt_test/client/EntryPointClass.java
@@ -1,12 +1,17 @@
 package rmugattarov.gwt_test.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import rmugattarov.gwt_test.server.MyRemoteService;
+import rmugattarov.gwt_test.server.MyRemoteServiceAsync;
 
 /**
  * Created by rmugattarov on 21.01.2016.
@@ -15,13 +20,22 @@ public class EntryPointClass implements EntryPoint {
 
     private Label label;
     private Button button;
+    private MyRemoteServiceAsync service = GWT.create(MyRemoteService.class);
 
     public void onModuleLoad() {
         label = new Label("Bazinga!");
         button = new Button("Hello!");
         button.addClickHandler(new ClickHandler() {
             public void onClick(ClickEvent clickEvent) {
-                label.setText(label.getText() + "a");
+                service.sayHello(new AsyncCallback<String>() {
+                    public void onFailure(Throwable throwable) {
+                        Window.alert("onFailure");
+                    }
+
+                    public void onSuccess(String s) {
+                        label.setText(s);
+                    }
+                });
             }
         });
 
