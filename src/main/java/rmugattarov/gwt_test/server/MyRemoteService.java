6b0c26bc846deaee97f399dc62ba859ac09470a8
rmugattarov <rmugattarov@RMugattarov2-PC.datateh.ru>
2016-01-21 18:56:57 +0500

    updates

--- src/main/java/rmugattarov/gwt_test/server/MyRemoteService.java
+++ src/main/java/rmugattarov/gwt_test/server/MyRemoteService.java
@@ -1,10 +1,12 @@
 package rmugattarov.gwt_test.server;
 
 import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 /**
  * Created by rmugattarov on 21.01.2016.
  */
+@RemoteServiceRelativePath("myService")
 public interface MyRemoteService extends RemoteService {
     String sayHello();
 }
--- src/main/java/rmugattarov/gwt_test/server/MyRemoteServiceServlet.java
+++ src/main/java/rmugattarov/gwt_test/server/MyRemoteServiceServlet.java
@@ -2,9 +2,12 @@ package rmugattarov.gwt_test.server;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
+import javax.servlet.annotation.WebServlet;
+
 /**
  * Created by rmugattarov on 21.01.2016.
  */
+@WebServlet("/myService")
 public class MyRemoteServiceServlet extends RemoteServiceServlet implements MyRemoteService {
     public String sayHello() {
         return "Hello!";
