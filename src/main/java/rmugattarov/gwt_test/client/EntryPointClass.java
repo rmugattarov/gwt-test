5af0544b0b77a7aef09c67c3a881a1e917337593
rmugattarov <rmugattarov@RMugattarov2-PC.datateh.ru>
2016-01-21 16:34:42 +0500

    updates

--- src/main/java/rmugattarov/gwt_test/client/EntryPointClass.java
+++ src/main/java/rmugattarov/gwt_test/client/EntryPointClass.java
@@ -3,12 +3,16 @@ package rmugattarov.gwt_test.client;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 /**
  * Created by rmugattarov on 21.01.2016.
  */
 public class EntryPointClass implements EntryPoint {
     public void onModuleLoad() {
-        RootPanel.get().add(new Label("Bazinga!"));
+        VerticalPanel verticalPanel = new VerticalPanel();
+        verticalPanel.add(new Label("Bazinga!"));
+        verticalPanel.add(new Label("Hello!"));
+        RootPanel.get().add(verticalPanel);
     }
 }
