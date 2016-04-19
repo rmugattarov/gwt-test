f3d364ad525338fa1831c22e281cf6b5f14d5504
rmugattarov <rmugattarov@RMugattarov2-PC.datateh.ru>
2016-04-19 20:29:04 +0500

    updates

--- src/main/java/rmugattarov/gwt_test/client/suggest_box_widget/SuggestBoxWidget.java
+++ src/main/java/rmugattarov/gwt_test/client/suggest_box_widget/SuggestBoxWidget.java
@@ -1,37 +1,72 @@
 package rmugattarov.gwt_test.client.suggest_box_widget;
 
-import com.google.gwt.event.logical.shared.SelectionEvent;
-import com.google.gwt.event.logical.shared.SelectionHandler;
-import com.google.gwt.event.logical.shared.ValueChangeEvent;
-import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.common.base.Strings;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.FocusEvent;
+import com.google.gwt.event.dom.client.FocusHandler;
 import com.google.gwt.user.client.ui.*;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.List;
 
 /**
  * Created by rmugattarov on 19.04.2016.
  */
 public class SuggestBoxWidget extends Composite {
+
+    private static class MyPopupPanel extends PopupPanel {
+        public MyPopupPanel(final TextBox textBox, List<String> elements) {
+            super(true);
+            setPopupPosition(textBox.getAbsoluteLeft(), textBox.getAbsoluteTop() + textBox.getOffsetHeight());
+            if (elements != null && !elements.isEmpty()) {
+                LinkedHashSet<String> validElements = new LinkedHashSet<String>();
+                for (String element : elements) {
+                    if (!Strings.isNullOrEmpty(element)) {
+                        validElements.add(element);
+                    }
+                }
+                if (!validElements.isEmpty()) {
+                    VerticalPanel verticalPanel = new VerticalPanel();
+                    for (String validElement : validElements) {
+                        final Label popupListElement = new Label(validElement);
+                        popupListElement.addClickHandler(new ClickHandler() {
+                            @Override
+                            public void onClick(ClickEvent clickEvent) {
+                                textBox.setText(popupListElement.getText());
+                                hide();
+                            }
+                        });
+                        verticalPanel.add(popupListElement);
+                    }
+                    setWidget(verticalPanel);
+                }
+            }
+        }
+
+        @Override
+        public void show() {
+            if (getWidget() != null) {
+                super.show();
+            }
+        }
+    }
+
     public SuggestBoxWidget() {
         VerticalPanel verticalPanel = new VerticalPanel();
-        MultiWordSuggestOracle oracle = new MultiWordSuggestOracle();
-        oracle.add("123");
-        oracle.add("234");
-        oracle.add("345");
-        SuggestBox suggestBox = new SuggestBox(oracle);
-        suggestBox.addSelectionHandler(new SelectionHandler<SuggestOracle.Suggestion>() {
+        final TextBox textBox = new TextBox();
+        textBox.addFocusHandler(new FocusHandler() {
             @Override
-            public void onSelection(SelectionEvent<SuggestOracle.Suggestion> selectionEvent) {
-                log("selectionEvent : " + selectionEvent.getSelectedItem().getReplacementString());
+            public void onFocus(FocusEvent focusEvent) {
+                MyPopupPanel popupPanel = new MyPopupPanel(textBox, Arrays.asList("123", "234", "345"));
+                popupPanel.show();
             }
         });
-        suggestBox.addValueChangeHandler(new ValueChangeHandler<String>() {
-            @Override
-            public void onValueChange(ValueChangeEvent<String> valueChangeEvent) {
-                log("valueChangeEvent : " + valueChangeEvent.getValue());
-            }
-        });
-        verticalPanel.add(suggestBox);
+        verticalPanel.add(textBox);
         initWidget(verticalPanel);
     }
+
 
     public static native void log(String msg) /*-{
         $wnd.console.log(msg);
